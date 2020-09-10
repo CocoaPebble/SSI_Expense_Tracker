040d069be7666619e2241c919e4efa63b0abffca
Haolin Yang <haolinyang95@gmail.com>
2020-09-09 22:21:13 -0700

    add redux and basic feature

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addTransaction } from '../slicers/transactionSlicer';
 
 export default function AddTransaction() {
-    return <div></div>;
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    dispatch(
+                        addTransaction({
+                            id: Math.random(),
+                            [e.target[0].name]: e.target[0].value,
+                            [e.target[1].name]: +e.target[1].value,
+                        })
+                    );
+                }}
+            >
+                <label>
+                    <input
+                        type="text"
+                        name="text"
+                        placeholder="input text"
+                        required
+                    />
+                </label>
+                <label>
+                    <input type="number" name="amount" required />
+                </label>
+                <button>add</button>
+            </form>
+        </div>
+    );
 }
--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function History() {
-    return <div></div>;
+    const { allTransactions } = useSelector((state) => state.transactions);
+    return (
+        <div>
+            <ul>
+                {allTransactions.map((t) => {
+                    return (
+                        <li key={t.id}>
+                            text: {t.text} --- amount: {t.amount}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
 }
