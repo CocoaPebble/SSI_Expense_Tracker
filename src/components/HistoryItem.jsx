c953e582ffd8b351e65e567b23a6c0226fd7d8a9
Haolin Yang <haolinyang95@gmail.com>
2020-09-11 16:28:37 -0700

    style delete button

--- src/components/HistoryItem.jsx
+++ src/components/HistoryItem.jsx
@@ -16,20 +16,27 @@ export default function HistoryItem({ data }) {
     return (
         <>
             <li>
-                <Paper
-                    className="historyItem"
-                    style={{
-                        borderRight:
-                            data.amount < 0
-                                ? '3px #f00 solid'
-                                : '3px #0f0 solid',
-                    }}
-                    square
-                >
-                    <div>{data.text}</div>
-                    <div>{data.amount}</div>
-                    <button onClick={() => handleDelete(data.id)}>del</button>
-                </Paper>
+                <div className="historyItemContainer">
+                    <Paper
+                        className="historyItem"
+                        style={{
+                            borderRight:
+                                data.amount < 0
+                                    ? '3px #f00 solid'
+                                    : '3px #0f0 solid',
+                        }}
+                        square
+                    >
+                        <div>{data.text}</div>
+                        <div>{data.amount}</div>
+                    </Paper>
+                    <span
+                        className="delBtn"
+                        onClick={() => handleDelete(data.id)}
+                    >
+                        &times;
+                    </span>
+                </div>
             </li>
         </>
     );
