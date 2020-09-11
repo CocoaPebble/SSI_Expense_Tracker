0afd356146509668c16a862086983844643bfdda
Haolin Yang <haolinyang95@gmail.com>
2020-09-10 17:45:17 -0700

    Updates

---
+++ precache-manifest.41b90212ceb86bd0ffeeb45035bcc0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6752735ce704295caa981e2efa2a5ad0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fdfc691515040d34754",
+    "url": "./static/css/main.df9d4b4b.chunk.css"
+  },
+  {
+    "revision": "bfa8c89262f39a6262d5",
+    "url": "./static/js/2.dbc30f6d.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "./static/js/2.dbc30f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fdfc691515040d34754",
+    "url": "./static/js/main.15f70f89.chunk.js"
+  },
+  {
+    "revision": "10b5e1bbd9d31c8411e0",
+    "url": "./static/js/runtime-main.18b5c38b.js"
+  }
+]);
